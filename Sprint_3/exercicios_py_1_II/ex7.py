83a0d10ac67556c086b298bcce50c1547fb9528d
gersonpn <gerson.neto@estudante.ifms.edu.br>
2023-08-20 23:31:17 -0400

    ex7

---
+++ Sprint_3/exercicios_py_1_II/ex7.py
@@ -0,0 +1,11 @@
+#Faça um programa que gere uma nova lista contendo apenas números ímpares.
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+b = []
+
+
+for n in a:
+    if n % 2 != 0:
+        b.append(n)
+print(b)
