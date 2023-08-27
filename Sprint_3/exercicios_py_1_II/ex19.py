497c5768ef1684b79e8aa2aae394394e61ae5799
gersonpn <gerson.neto@estudante.ifms.edu.br>
2023-08-26 23:11:15 -0400

    ex 19 e 20

--- Sprint_3/exercicios_py_1_II/ex19.py
+++ Sprint_3/exercicios_py_1_II/ex19.py
@@ -1,21 +1,26 @@
+
+
 import random
 
-
 random_list = random.sample(range(500), 50)
-print(random_list)
 
 for n in random_list:
     random_list = sorted(random_list)
 
 
-print(random_list)
-
-
 mediana = 0
 
+metade = len(random_list) // 2
 
-media = (sum(random_list)) // len(random_list)
+if len(random_list) % 2 == 1:
+    mediana = random_list[metade]
+else:
+    mediana = (random_list[metade - 1] + random_list[metade]) /2
+
+
+
+media = (sum(random_list)) / len(random_list)
 valor_minimo = (min(random_list))
 valor_maximo = (max(random_list))
 
-print(valor_minimo, valor_maximo, media)
+print (f"Media: {media}, Mediana: {mediana}, Mínimo: {valor_minimo}, Máximo: {valor_maximo}")
