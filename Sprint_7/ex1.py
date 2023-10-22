39f8f806797d8580231ee4677b6bba976cd7c5ca
gersonpn <gerson.neto@estudante.ifms.edu.br>
2023-10-22 02:05:19 -0400

    ex3

--- Sprint_7/ex1.py
+++ Sprint_7/ex1.py
@@ -21,3 +21,18 @@ print(f'O ator/atriz com o maior número de filmes é {ator_filme} com {number_o
 # 2 - Apresente a média da coluna contendo o número de filmes.
 
 print(f'A média do número de filmes é: {n_filme.mean():.2f}')
+
+
+# 3 - Apresente o nome do ator/atriz com a maior média por filme.
+
+# Calcula a média por filme
+data['Average per Movie'] = data['Total Gross'] / data['Number of Movies']
+
+# Encontre o índice do ator/atriz com a maior média por filme
+indice_max = data['Average per Movie'].idxmax()
+
+# Acesse as informações do ator/atriz com a maior média por filme
+actor_maior = data.loc[indice_max, 'Actor']
+media = data.loc[indice_max, 'Average per Movie']
+
+print(f'O ator/atriz com a maior média por filme é {actor_maior} com uma média de {media:.2f} por filme.')
