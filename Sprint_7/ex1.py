a930b89d06222d9ba6fed9f09c6b1acafeaf1136
gersonpn <gerson.neto@estudante.ifms.edu.br>
2023-10-22 02:12:58 -0400

    ex4

--- Sprint_7/ex1.py
+++ Sprint_7/ex1.py
@@ -36,3 +36,21 @@ actor_maior = data.loc[indice_max, 'Actor']
 media = data.loc[indice_max, 'Average per Movie']
 
 print(f'O ator/atriz com a maior média por filme é {actor_maior} com uma média de {media:.2f} por filme.')
+
+
+# 4 - Apresente o nome do(s) filme(s) mais frequente(s) e sua respectiva frequência.
+
+#Value_counts() para contar a frequência que aparece cada filme
+
+freq_filme = data['#1 Movie'].value_counts()
+
+mais_freq = freq_filme [freq_filme == freq_filme.max()]
+
+# Acesse o nome do filme mais frequente e sua frequência
+
+nome_filme = mais_freq.index.tolist()
+freq_filme = mais_freq.max()
+
+
+for filme in nome_filme:
+    print(f"O filme mais frequente {filme}: {freq_filme} vezes")
